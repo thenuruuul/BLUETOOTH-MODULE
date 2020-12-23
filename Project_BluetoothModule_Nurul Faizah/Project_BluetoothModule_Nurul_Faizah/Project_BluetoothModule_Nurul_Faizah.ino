2611471d34a68195bdad0cf7f046116d808cfe42
thenuruuul <73013649+thenuruuul@users.noreply.github.com>
2020-12-23 21:41:59 +0700

    Add files via upload

---
+++ Project_BluetoothModule_Nurul Faizah/Project_BluetoothModule_Nurul_Faizah/Project_BluetoothModule_Nurul_Faizah.ino
@@ -0,0 +1,22 @@
+int ml=A0;
+int h=255;
+int l=0;
+String motion;
+void setup() {
+  Serial.begin(9600);
+  pinMode(ml, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Silahkan ketik 'p' untuk memutar, 's' untuk berhenti");
+  while(Serial.available()==0){ }
+  motion=Serial.readString();
+  if(motion=="p") {
+    analogWrite(ml,l);
+    }
+  if(motion=="s") {
+    analogWrite(ml,h);
+    }
+}
